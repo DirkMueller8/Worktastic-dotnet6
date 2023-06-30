1fa9ab481ae2ea7aaf74cde018d1b73031646f75
Dirk Mueller <dirkmuller8@outlook.de>
2023-06-30 15:07:37 +0200

    Add project files.

---
+++ Worktastic/Controllers/JobPostingController.cs
@@ -0,0 +1,125 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Worktastic.Data;
+using Worktastic.Models;
+
+namespace Worktastic.Controllers
+{
+    [Authorize]
+    public class JobPostingController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public JobPostingController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            if (User.IsInRole("Admin"))
+            {
+                var allPostings = _context.JobPostings.ToList();
+                return View(allPostings);
+            }
+
+            var jobPostingsFromDb = _context.JobPostings.Where(x => x.OwnerUsername == User.Identity.Name).ToList();
+
+            return View(jobPostingsFromDb);
+        }
+
+        public IActionResult CreateEditJobPosting(int id)
+        {
+            if (id != 0)
+            {
+                var jobPostingFromDb = _context.JobPostings.SingleOrDefault(x => x.Id == id);
+
+                if ((jobPostingFromDb.OwnerUsername != User.Identity.Name) && !User.IsInRole("Admin"))
+                {
+                    return Unauthorized();
+                }
+
+                if (jobPostingFromDb != null)
+                {
+                    return View(jobPostingFromDb);
+                }
+                else
+                {
+                    return NotFound();
+                }
+            }
+
+            return View();
+        }
+
+        public IActionResult CreateEditJob(JobPosting jobPosting, IFormFile file)
+        {
+            jobPosting.OwnerUsername = User.Identity.Name;
+            if (file != null)
+            {
+                using (var ms = new MemoryStream())
+                {
+                    file.CopyTo(ms);
+                    var bytes = ms.ToArray();
+                    jobPosting.CompanyImage = bytes;
+                }
+            }
+
+            if (jobPosting.Id == 0)
+            {
+                _context.JobPostings.Add(jobPosting);
+            }
+            else
+            {
+                // Entity Framework query, whereby search and give us that element which we had
+                // given above (jobPosting.Id):
+                var jobFromDb = _context.JobPostings.SingleOrDefault(x => x.Id == jobPosting.Id);
+                if (jobFromDb == null)
+                {
+                    // Im Falle eines schweren Pogrammierfehlers gib eine 404-Seite aus:
+                    return NotFound();
+                }
+
+                jobFromDb.CompanyImage = jobPosting.CompanyImage;
+                jobFromDb.CompanyName = jobPosting.CompanyName;
+                jobFromDb.ContactMail = jobPosting.ContactMail;
+                jobFromDb.ContactPhone = jobPosting.ContactPhone;
+                jobFromDb.ContactWebsite = jobPosting.ContactWebsite;
+                jobFromDb.Description = jobPosting.Description;
+                jobFromDb.JobLocation = jobPosting.JobLocation;
+                jobFromDb.JobTitle = jobPosting.JobTitle;
+                jobFromDb.Salary = jobPosting.Salary;
+                jobFromDb.StartDate = jobPosting.StartDate;
+                // jobFromDb.OwnerUsername = jobPosting.OwnerUsername;
+            }
+            // Add new job if not editing:
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public IActionResult DeleteJobPostingById(int id)
+        {
+            if (id == 0)
+                return BadRequest();
+
+            var jobPostingsFromDb = _context.JobPostings.SingleOrDefault(x => x.Id == id);
+
+            if (jobPostingsFromDb == null)
+            {
+                return NotFound();
+            }
+            _context.JobPostings.Remove(jobPostingsFromDb);
+            _context.SaveChanges();
+
+            return Ok();
+        }
+    }
+}
